9dc266dda249bd94c9f4af198ecd1d8911154877
conradhale <halec5@mail.uc.edu>
2022-09-22 14:35:11 -0400

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Enter your age: ");
+    int age = 20;
+
+    if (age >= 21) {
+      System.out.println("You get a paper wristband");
+    }
+  }
+}
